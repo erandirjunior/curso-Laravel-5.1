be611c4d5293f31b947a5125c1987c51015aa185
fsoaresjunior <aefsj1234>
2017-06-01 10:58:44 -0300

    Recuperando senha no laravel

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -7,6 +7,8 @@ use Illuminate\Foundation\Auth\ResetsPasswords;
 
 class PasswordController extends Controller
 {
+    protected $redirectTo = '/painel/carros';
+
     /*
     |--------------------------------------------------------------------------
     | Password Reset Controller
