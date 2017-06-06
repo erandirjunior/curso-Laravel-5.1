43976cfbd52cb3866d04157ee7684ed1084d1868
fsoaresjunior <aefsj1234>
2017-06-06 19:49:46 -0300

    Implantando o Dashboard do painel.

--- resources/views/painel/home/index.blade.php
+++ resources/views/painel/home/index.blade.php
@@ -1,1 +1,7 @@
-Painel do site
+@extends('painel.templates.index')
+
+@section('content')
+
+    Home do Painel - Relatórios.
+
+@endsection
--- resources/views/painel/templates/index.blade.php
+++ resources/views/painel/templates/index.blade.php
@@ -2,24 +2,83 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <title>{{$titulo or 'Curso de Laravel 5.1'}}</title>
-    <link rel="stylesheet" href="/assets/css/bootstrap.css">
+    <title>{{$titulo or 'Painel | Curso de Laravel 5.1'}}</title>
+
+    <!-- Latest compiled and minified CSS -->
+    {!!HTML::style('assets/css/bootstrap.min.css')!!}
+
+    <!-- Optional theme -->
+    {!!HTML::style('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css')!!}
+    {!!HTML::style('//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css')!!}
+    {!!HTML::style('assets/painel/css/especializati.css')!!}
+    {!!HTML::style('assets/painel/css/especializati-responsivo.css')!!}
+
+    <!--JQuery-->
+    {!!HTML::script('assets/js/jquery.js')!!}
 </head>
-<body>
-<div class="container">
-    @section('slide')
-        slide
-    @show <!-- Exibe -->
+<body class="bg-padrao">
 
-    <!-- é usada para exibir o conteúdo de uma determinada seção -->
-    @yield('content')
+<header>
+    <h1 class="oculta">{{$titulo or 'Painel | Curso de Laravel 5.1'}}</h1>
+</header>
 
-</div>
-<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<section class="painel">
+    <h1 class="oculta">Painel | EspecializaTi</h1>
 
-    {{--Insere os scripts js de forma dinâmica--}}
-    @yield('scripts')
+    <div class="topo-painel col-md-12">
+        <a href="" class="icon-acoes-painel">
+            <i class="fa fa-expand"></i>
+        </a>
 
+        {!!HTML::image('assets/imgs/logo-especializati.png', 'EspecializaTi', ['class' => 'logo-painel', 'title' => 'EspecializaTi - Curso de Laravel 5.1'])!!}
+
+        <select class="acoes-painel">
+            <option>{{Auth::user()->name}}</option>
+            <option class="sair">Sair</option>
+        </select>
+    </div>
+    <!--End Top-->
+
+    <div class="clear"></div>
+
+
+    <div class="menu-painel col-md-2">
+        <ul class="menu-painel-ul">
+            <li>
+                <a href=""><i class="fa fa-tachometer"></i> Dashboard</a>
+            </li>
+            <li>
+                <a href=""><i class="fa fa-user-secret"></i> Meu Perfil</a>
+            </li>
+            <li>
+                <a href=""><i class="fa fa-user"></i> Alunos</a>
+            </li>
+            <li>
+                <a href=""><i class="fa fa-user-times"></i> Pais</a>
+            </li>
+            <li>
+                <a href=""><i class="fa fa-users"></i> Usuários</a>
+            </li>
+            <li>
+                <a href="?pag=produtos"><i class="fa fa-diamond"></i> Produtos</a>
+            </li>
+            <li>
+                <a href=""><i class="fa fa-sign-out"></i> Sair</a>
+            </li>
+        </ul>
+    </div>
+    <!--End menu-->
+
+    <section class="conteudo col-md-10">
+        <div class="cont">
+            @yield('content')
+        </div>
+    </section>
+    <!--End Conteúdo-->
+</section>
+
+
+<!-- Latest compiled and minified JavaScript -->
+{!!HTML::script('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js')!!}
 </body>
 </html>
